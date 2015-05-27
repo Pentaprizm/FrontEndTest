eea4add54ad96dd783c8b09a19f1cbb48bf290a6
Yuri Solovyov <yuriandsolo@gmail.com>
2015-05-27 22:07:20 +0300

    add form

--- script/script.js
+++ script/script.js
@@ -1,2 +1,12 @@
 $("#sendReq").bind("click", function(){$("#sendReq").addClass("hidden");
 	$(".btn-wrapper").append("<img src='img/ajax-loader.gif'>");});
+	
+	
+function reqListener() {
+  var data = JSON.parse(this.responseText);    
+}
+
+var oReq = new XMLHttpRequest();  
+oReq.onload = reqListener;
+oReq.open('GET', 'http://www.json-generator.com/api/json/get/bUsRkvEmHm?indent=2', true);
+oReq.send();
